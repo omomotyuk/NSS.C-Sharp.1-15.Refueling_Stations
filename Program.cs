32801113ba96fcf4fae0bac4ddb8a1a59c71bb77
Alex Momotyuk <o.a.momotyuk@gmail.com>
2019-11-26 11:41:26 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_15.Refueling_Stations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# NSS.C-Sharp.1-15.Refueling_Stations
